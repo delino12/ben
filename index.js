c6a0b6b459f8e3381455b61c1b055f077950bd58
Ekpoto Liberty Bernard <ekpoto.liberty@gmail.com>
2019-12-14 15:09:09 +0100

    add dynamic commit with prompt

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 #!/usr/bin/env node
-
-var programmer = require('commander');
-var repository = require('./src/ben').repository;
-var status = require('./src/ben').status;
+var inquirer = require('inquirer');
+var programmer 	= require('commander');
+var repository 	= require('./src/ben').repository;
+var status 		= require('./src/ben').status;
+var greet 		= require('./src/ben').greet;
+var commit      = require('./src/ben').commit;
 
 programmer.version('1.0.0').description('Welcome to Ben git helper');
-programmer.command('which repo').alias('wr').description('track changes').action(() => {
+programmer.command('hello').alias('hi').description('Greetings').action(() => {
+	greet();
+});
+
+programmer.command('which repo').alias('wr').description('Show the current repository you are working on').action(() => {
 	repository();
 });
 
-programmer.command('show status').alias('ss').description('track changes').action(() => {
+programmer.command('show status').alias('ss').description('Show project status and changes').action(() => {
 	status();
+});
+
+programmer.command('save this').alias('ss').description('Track changes and request commit message').action(() => {
+	// commit();
+	inquirer.prompt([{
+		type: 'input',
+		name: 'message',
+		message: 'What will you like to call this change?'
+	}]).then(function (answers) {
+        console.log(answers);
+        console.log('committing...');
+        commit(answers.message);
+    })
 });
 
 programmer.parse(process.argv);
--- src/ben.js
+++ src/ben.js
@@ -1,11 +1,12 @@
 var exec = require('child_process').exec;
-var inquirer = require('inquirer');
 var colors = require('colors');
 
 var repository = () => {
 	exec('git remote -v', (error, stdout, stderr) => {
-	    console.log(`stdout: ${stdout}`);
-	    console.warn(`stderr: ${stderr}`);
+	    console.info(`Here is what i found!: \n ${stdout}`);
+	    if(stderr){
+	    	console.warn(`stderr: ${stderr}`);
+	    }
 	    if (error !== null) {
 	        console.log(`exec error: ${error}`);
 	    }
@@ -14,12 +15,40 @@ var repository = () => {
 
 var status = () => {
 	exec('git status', (error, stdout, stderr) => {
-	    console.log(`stdout: ${stdout}`);
-	    console.warn(`stderr: ${stderr}`);
+	    console.log(`${stdout}`);
+	    if(stderr){
+	    	console.warn(`stderr: ${stderr}`);
+	    }
 	    if (error !== null) {
 	        console.log(`exec error: ${error}`);
 	    }
 	});
 }
 
-module.exports = {repository, status};
+var greet = () => {
+	console.info(`Hi there, I'm ${colors.grey('Ben')} i can help you perform some cool friendly git command task, to get started type: ${colors.grey('Ben --help')}`);
+}
+
+var commit = (message) => {
+	exec(`git add .`, (error, stdout, stderr) => {
+	    console.log(`tracking....`);
+	    if(stderr){
+	    	console.warn(`stderr: ${stderr}`);
+	    }
+	    if (error !== null) {
+	        console.log(`exec error: ${error}`);
+	    }
+	});
+
+	exec(`git commit -m "${message}"`, (error, stdout, stderr) => {
+	    console.log(`${stdout}`);
+	    if(stderr){
+	    	console.warn(`stderr: ${stderr}`);
+	    }
+	    if (error !== null) {
+	        console.log(`exec error: ${error}`);
+	    }
+	});
+}
+
+module.exports = {repository, status, commit, greet};
